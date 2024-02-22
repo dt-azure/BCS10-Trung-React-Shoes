cc54d43144a4a9fdfe6235521ed00d1824e713a3
Trung Nguyen <dtrung2702@gmail.com>
2024-02-22 09:52:07 +0700

    removed commented code

--- src/Shoes/Shoes.jsx
+++ src/Shoes/Shoes.jsx
@@ -5,63 +5,6 @@ import Cart from "./Cart";
 import { connect } from "react-redux";
 
 export default class Shoes extends Component {
-  // renderItems = () => {
-  //   return this.state.shoesArr.map((item) => (
-  //     <Item name={item.name} imgSrc={item.image} price={item.price} />
-  //   ));
-  // };
-
-  // handleChangeDetails = (item) => { 
-  //   this.setState({
-  //       details: item,
-  //   })
-  //  };
-
-  // handleAddToCart = (item) => {
-  //   let newCart = [...this.state.cart]
-
-  //   let index = newCart.findIndex((n) => {
-  //     return n.id == item.id
-  //   })
-
-  //   if (index == -1) {
-  //     let newItem = { ...item, amount: 1 }
-  //     newCart.push(newItem)
-  //   }
-  //   else {
-  //     newCart[index].amount++
-  //   }
-
-  //   this.setState({
-  //     cart: newCart,
-  //   });
-    
-  //  };
-
-  // handleChangeAmount = (id, k) => { 
-  //   let newCart = [...this.state.cart]
-  //   let index = newCart.findIndex((item) => {
-  //     return item.id == id
-  //   })
-
-  //   if (k == 0) {
-  //     newCart[index].amount = 0;
-  //   }
-  //   else {
-  //     newCart[index].amount += k;
-  //   }
-
-  //   if (newCart[index].amount == 0) {
-  //     newCart.splice(index,1)
-  //   }
-    
-  //   this.setState({
-  //     cart: newCart,
-  //   });
-
-  //  };
-
-
   render() {
     return (
       <div className="p-5 text-center">
